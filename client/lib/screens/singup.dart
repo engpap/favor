5e964795451c0408f3f8a291924350e8de135f50
Nicholas Nicolis <nicholas.nicolis@gmail.com>
2022-10-04 13:09:54 +0200

    singnup [+ text fields]

--- client/lib/screens/singup.dart
+++ client/lib/screens/singup.dart
@@ -9,19 +9,50 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _textController1 =
+      TextEditingController(text: "name");
+  final TextEditingController _textController2 =
+      TextEditingController(text: "surname");
+  final TextEditingController _textController3 =
+      TextEditingController(text: "email");
+  final TextEditingController _textController4 =
+      TextEditingController(text: "password");
+  final TextEditingController _textController5 =
+      TextEditingController(text: "Confirm password");
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       backgroundColor: CupertinoColors.lightBackgroundGray,
       child: Center(
-        child: CupertinoButton(
-          child: const Text("go homeScreen"),
-          onPressed: () {
-            Navigator.push(
-              context,
-              CupertinoPageRoute(builder: (context) => const HomeScreen()),
-            );
-          },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            CupertinoButton(
+              child: const Text("go homeScreen"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  CupertinoPageRoute(builder: (context) => const HomeScreen()),
+                );
+              },
+            ),
+            CupertinoTextField(
+              controller: _textController1,
+            ),
+            CupertinoTextField(
+              controller: _textController2,
+            ),
+            CupertinoTextField(
+              controller: _textController3,
+            ),
+            CupertinoTextField(
+              controller: _textController4,
+            ),
+            CupertinoTextField(
+              controller: _textController5,
+            ),
+          ]),
         ),
       ),
     );
