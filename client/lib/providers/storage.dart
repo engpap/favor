516676d9de58f24c93856f118f06c8da68dde88f
engpap <10701904@polimi.it>
2022-12-23 19:55:31 +0100

    getToken for iOS

--- client/lib/providers/storage.dart
+++ client/lib/providers/storage.dart
@@ -2,8 +2,9 @@ import 'dart:io' show Platform;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Storage {
-  static String _PlatformNotSupported = 'platform not supported';
+  //static String _PlatformNotSupported = 'platform not supported';
 
+  /*
   static Future<String?> getUserToken() async {
     if (Platform.isAndroid) {
       print(">>> Returning token stored in storage");
@@ -14,5 +15,17 @@ class Storage {
     } else {
       return _PlatformNotSupported;
     }
+  }
+  */
+  static Future<String?> getUserToken() async {
+    print(">>> Returning token stored in storage");
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    return _prefs.getString('token');
+  }
+
+  static void setUserToken(String token) async {
+    print(">>> Storing token in storage");
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    _prefs.setString('token', token);
   }
 }
--- client/lib/screens/account.dart
+++ client/lib/screens/account.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:project/functions/tabs.dart';
 import 'package:project/providers/getters.dart';
+import 'package:project/providers/storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../errors/errorConstants.dart';
 import 'components/customField.dart';
@@ -15,17 +17,6 @@ class AccountScreen extends StatefulWidget {
 }
 
 class _AccountScreenState extends State<AccountScreen> {
-  final String _PlaceholderName = "Name";
-  final String _PlaceholderSurname = "Surname";
-
-  final TextEditingController _textControllerName =
-      TextEditingController(text: "");
-  final TextEditingController _textControllerSurname =
-    TextEditingController(text: "");
-
-  String _StatusName = ErrorConstants.UNCHECKED_ERROR;
-  String _StatusSurname = ErrorConstants.UNCHECKED_ERROR;
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -37,32 +28,19 @@ class _AccountScreenState extends State<AccountScreen> {
       },
       child: Container(
         child: SafeArea(
-          child: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Divider(height: 20, color: Colors.transparent,),
-              /// NAME
-              Text("name", style: TextStyle(fontWeight: FontWeight.bold),),
-              CustomField(
-                prefixIcon: CupertinoIcons.profile_circled,
-                placeholder: _PlaceholderName,
-                textController: _textControllerName,
-                status: _StatusName,
-              ),
-              Divider(height:5, color: Colors.transparent,),
-              /// SURNAME
-              Text("Surname", style: TextStyle(fontWeight: FontWeight.bold),),
-              CustomField(
-                prefixIcon: CupertinoIcons.profile_circled,
-                placeholder: _PlaceholderSurname,
-                textController: _textControllerSurname,
-                status: _StatusSurname,
-                textInputAction: TextInputAction.done,
-              ),
-            ],
-          )),
-        ),
+            child: Center(
+          child: FutureBuilder<String?>(
+              future: Storage.getUserToken(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text('Token got from server: ${snapshot.data!}');
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                // By default, show a loading spinner.
+                return CupertinoActivityIndicator(animating: false, radius: 10);
+              }),
+        )),
       ),
     );
   }
