7663722afacf7dff31759d368941f4c01daaf88d
engpap <10701904@polimi.it>
2022-12-30 16:05:27 +0100

    getBookedFavors BackEnd

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,15 +1,17 @@
 import express from 'express';
-import { createPost, getPosts, getPostsBySearch,getFavorConstants, bookFavor} from '../controllers/posts.js';
+import { createPost, getPosts, getPostsBySearch,getFavorConstants, bookFavor, getBookedFavors} from '../controllers/posts.js';
 import auth from '../middleware/auth.js'
 
 const router = express.Router();
 
-router.post('/bookFavor', auth,bookFavor)
+
+router.get('/getBookedFavors', getBookedFavors);
 router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
 router.get('/getFavorConstants', getFavorConstants);
 //router.get('/:id', getPost);
 
+router.post('/bookFavor', auth,bookFavor)
 router.post('/create', auth, createPost);
 
 export default router;
