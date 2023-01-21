4b2ff132ee7a73f11ed682657403ec89e12c8d1e
engpap <10701904@polimi.it>
2023-01-21 15:22:54 +0100

    minor changes

--- client/lib/models/bookedFavor.dart
+++ client/lib/models/bookedFavor.dart
@@ -4,16 +4,6 @@ import 'package:project/models/callerPost.dart';
 import 'package:project/models/post.dart';
 import 'package:project/models/providerPost.dart';
 
-/**bookedAt: { //not in body; generated from server as a ISO format string
-        type: Date,
-        default: new Date(),
-    },
-    providerId: { type: String, required: true },
-    callerId:  { type: String, required: true },
-    post: { type: Schema.Types.ObjectId, ref: 'Post' },
-    isTerminated: { type: Boolean, default:false, required: true},
-    */
-
 class BookedFavor {
   final String id;
   final DateTime bookedAt;
--- client/lib/providers/getters.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:project/models/user.dart';
-import 'package:provider/provider.dart';
-import 'package:project/providers/userProvider.dart';
-
-User getUserFromlocalStorage(BuildContext context) =>
-    Provider.of<UserProvider>(context).user;
