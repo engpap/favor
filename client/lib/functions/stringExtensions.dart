1b65c49c7423f45e4acdc320833817dbd1db2c88
engpap <10701904@polimi.it>
2023-01-21 14:20:30 +0100

    string extensions isValidPassword method

--- client/lib/functions/stringExtensions.dart
+++ client/lib/functions/stringExtensions.dart
@@ -11,24 +11,30 @@ extension StringValidator on String {
 
   bool isValidInt() => int.tryParse(this) != null;
 
-  //TODO: creare criteri per cui la password non è valida (tipo lunghezza, caratteri, ecc)
-  bool isValidPassword() => this.isWhitespace();
+  /// Check if the password is at least 8 characters long and contains
+  /// at least one uppercase letter, one lowercase letter, and one digit.
+  bool isValidPassword() =>
+      (new RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$')).hasMatch(this);
 
+  // TODO: TO REMOVE, THERE IS SERVER DATA!
   // true: any number between 0 and 99
   bool isValidAge() {
     return RegExp(r'^([0-9]{1,2})$').hasMatch(this);
   }
 
+  // TODO: TO REMOVE, THERE IS SERVER DATA!
   // true: if 'M', 'm', 'F' or 'f'
   bool isValidGender() {
     return RegExp(r'^[MmFf]$').hasMatch(this);
   }
 
+  // TODO: TO REMOVE, THERE IS SERVER DATA!
   // true: if letters A-z, numbers 0-9, dot . or comma ,
   bool isValidResidence() {
     return RegExp(r'^[A-z.,\s0-9]+$').hasMatch(this);
   }
 
+  // TODO: TO REMOVE, THERE IS SERVER DATA!
   // true: if string contains just letters
   bool isValidJob() {
     return RegExp(r'^[A-z]+$').hasMatch(this);
