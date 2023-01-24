b9ecb19580c43d1c82351feb0e2624a98d313d06
Nicholas Nicolis <nicholas.nicolis@gmail.com>
2023-01-24 19:38:59 +0100

    removed useless code

--- client/lib/functions/responsive.dart
+++ client/lib/functions/responsive.dart
@@ -9,10 +9,6 @@ class Responsive {
     return MediaQuery.of(context).size.height * (p / 100);
   }
 
-  static widthAuto(BuildContext context) {
-    //TODO distinguere tra tablet e phone, if width > x pixel return bigWidth else smallWidth
-  }
-
   // Use this function to set width of column (that refers to phone layouts) in tablet layouts
   static fixedWidth() {
     return 370.toDouble();
--- client/lib/functions/tabs.dart
+++ client/lib/functions/tabs.dart
@@ -4,7 +4,6 @@ import 'package:project/screens/explore/explore.dart';
 import 'package:project/screens/favor/favor.dart';
 import 'package:project/screens/feed/feed.dart';
 import 'package:project/screens/leaderboard/leaderboard.dart';
-import 'package:project/functions/favorColors.dart' as favorColors;
 
 /// Enumeration of the Tabs present in the BottomNavigationBar
 enum TabsName { Home, Explore, Favor, Leaderboard, Account }
