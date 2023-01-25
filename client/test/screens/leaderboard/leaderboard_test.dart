226971ca803632644c503dc3c9b55a0b7b3d4e86
engpap <10701904@polimi.it>
2023-01-25 22:34:19 +0100

    tests on leaderboard

---
+++ client/test/screens/leaderboard/leaderboard_test.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:project/providers/user_provider.dart';
+import 'package:project/screens/leaderboard/leaderboard.dart';
+
+import 'package:project/functions/favorColors.dart' as favorColors;
+import '../../utility_for_testing/utilities.dart';
+
+void main() {
+  testWidgets(
+      'LeaderboardCard should return a CupertinoButton widget with correct color',
+      (tester) async {
+    var leaderboardCard = LeaderboardCard(
+      personImage: MemoryImage(base64Decode(noProfilePicture64String)),
+      personName: 'Mario',
+      personSurname: 'Rossi',
+      leaderboardPosition: 1,
+      starsNumber: 3,
+    );
+    await tester.pumpWidget(getApp(leaderboardCard));
+
+    final cupertinoButtonFinder = find.byType(CupertinoButton);
+    final cupertinoButton =
+        cupertinoButtonFinder.evaluate().first.widget as CupertinoButton;
+    expect(cupertinoButton.color, equals(favorColors.IntroBg));
+  });
+}
